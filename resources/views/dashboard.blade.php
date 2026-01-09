b9f17a91ffefb5864fde9e10d3f581844c769070
eduardo <edudbdaw@gmail.com>
2026-01-09 23:22:24 +0000

    Prueba mostrar un producto

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,15 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h3 class="font-bold text-lg text-blue-600">Nombre del Producto:</h3>
+                    {{-- Mostramos el nombre simple --}}
+                    @dump($producto)
 
-                    @dump($datas);
+                    <h3 class="font-bold text-lg text-red-600 mt-4">Ficha Nutricional (Lo complejo):</h3>
+                    {{-- Mostramos el objeto gigante con los nutrientes --}}
+                    @dump($nutrientes)
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,26 @@ use StaticKidz\BedcaAPI\BedcaClient;
 Route::redirect('/', 'dashboard');
 
 Route::get('/dashboard', function () {
-
     $client = new BedcaClient();
-    $rawDatas = $client->getFoodGroups();
 
-    return view('dashboard' , ['datas' => $rawDatas]);
+    // 1. Primero pedimos la lista de productos de la categoría "Frutas" (El ID 5 en Bedca)
+    $listaProductos = $client->getFoodsInGroup(5);
+
+    // 2. Vamos a coger el PRIMER producto que aparezca en esa lista (será aleatorio, una manzana, cereza...)
+    // El objeto tiene una propiedad 'food' que es la lista, cogemos el índice [0]
+    $primerProducto = $listaProductos->food[0];
+    
+    // Sacamos su ID (f_id)
+    $idDeLaComida = $primerProducto->f_id;
+
+    // 3. AQUÍ ESTÁ LA MAGIA: Pedimos la ficha nutricional completa de ese ID
+    $fichaCompleta = $client->getFood($idDeLaComida);
+
+    return view('dashboard', [
+        'producto' => $primerProducto, // Para ver el nombre
+        'nutrientes' => $fichaCompleta // Para ver la info detallada
+    ]);
+
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -22,3 +37,10 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+/* Show all products
+    $client = new BedcaClient();
+    $rawDatas = $client->getFoodGroups();
+
+    return view('dashboard' , ['datas' => $rawDatas]);
+*/ 
