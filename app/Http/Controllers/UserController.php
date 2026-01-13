1505016ceae2a2f8b7f2beeede6197e87c4cd923
EduClase <edudbdawclase@gmail.com>
2026-01-13 17:13:58 +0100

    User api token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    function index(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if ($user && password_verify($request->password, $user->password)) {
+            return response()->json([
+                'status' => true,
+                'message' => 'Usuario autenticado',
+                'token' => $user->createToken('api_token')->plainTextToken,
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Credenciales incorrectas',
+                'token' => null,
+            ]);
+        }
+    }
 }
